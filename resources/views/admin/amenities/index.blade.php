648c09173746a3d75853bc1786cefa2769806ab4
Muzammil <muzammilshahzad894@gmail.com>
2024-09-24 12:10:01 +0500

    improvements in amenities

--- resources/views/admin/amenities/index.blade.php
+++ resources/views/admin/amenities/index.blade.php
@@ -12,11 +12,11 @@ Manage Amenities
     <!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-lg-12 col-md-12">
+            <div class="col-md-12">
                 <div class="box">
                     <div class="box-header">
                         <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="col-md-6">
                                 <h3 class="box-title">Amenities</h3>
                                 <div class="m-t-10">
                                     <a href="{{ url('admin/amenities/create') }}" class="btn btn-success">Add Amenity</a>
@@ -26,32 +26,28 @@ Manage Amenities
                     </div>
 
                     <div class="box-body">
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                                <table class="table table-striped table-hover m-b-20">
-                                    <tr>
-                                        <th>Group</th>
-                                        <th>Name</th>
-                                        <th>Type</th>
-                                        <th class="text-center">Action</th>
-                                    </tr>
-                                    @foreach($amenities as $amenity)
-                                    <tr data-delete="{{ url('admin/amenities/delete') }}/{{$amenity->id}}">
-                                        <td>{{ $amenity->group }}</td>
-                                        <td>{{ $amenity->title }}</td>
-                                        <td>{{ $amenity->type }}</td>
+                        <table class="table table-responsive table-bordered table-hover">
+                            <tr>
+                                <th>Group</th>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th class="text-center">Actions</th>
+                            </tr>
+                            @foreach($amenities as $amenity)
+                            <tr data-delete="{{ url('admin/amenities/delete') }}/{{$amenity->id}}">
+                                <td>{{ $amenity->group }}</td>
+                                <td>{{ $amenity->title }}</td>
+                                <td>{{ $amenity->type }}</td>
 
-                                        <td class="text-center">
-                                            <a href="{{ url('admin/amenities') }}/{{ $amenity->id }}/edit"><i class="fa fa-pencil"></i></a>
-                                            <span class="">
-                                                <a data-delete-trigger href="#"><i class="fa fa-trash"></i></a>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </table>
-                            </div>
-                        </div>
+                                <td class="text-center">
+                                    <a data-toggle="tooltip" title="Edit" href="{{ url('admin/amenities') }}/{{ $amenity->id }}/edit" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
+                                    <a data-toggle="tooltip" title="Delete" data-delete-trigger href="#" class="btn btn-danger btn-xs">
+                                        <i class="fa fa-remove"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
                     </div>
                     <!-- /.box-body -->
                 </div>
--- resources/views/admin/sleeps/create.blade.php
+++ resources/views/admin/sleeps/create.blade.php
@@ -12,11 +12,11 @@ Add Sleep
     <!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-lg-12 col-md-12">
+            <div class="col-md-12">
                 <div class="box">
                     <div class="box-header">
                         <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="col-md-6">
                                 <h3 class="box-title">Add Sleep</h3>
                                 <div class="m-t-10">
                                     <a class="btn btn-default" href="{{ url('admin/sleeps') }}">Back</a>
@@ -29,15 +29,29 @@ Add Sleep
                         <div id="show-messages"></div>
                         <div class="row">
                             <form action="{{ url('admin/sleep') }}" method="POST" role="form" enctype="multipart/form-data" class="ajax-submission">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                                @csrf
+                                <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="title" class="field-required">Value</label>
-                                        <input type="text" onkeypress='return event.charCode >= 48 && event.charCode <= 57' class="form-control input-half" id="title" name="title" value="{{ old('title') }}" placeholder="">
+                                        <input 
+                                            type="text" 
+                                            onkeypress='return event.charCode >= 48 && event.charCode <= 57' 
+                                            class="form-control input-half" 
+                                            id="title" 
+                                            name="title" 
+                                            value="{{ old('title') }}"
+                                        >
                                     </div>
                                     <div class="form-group input-3rd">
                                         <label for="display_order" class="field-required">Display Order </label>
-                                        <input type="text" class="form-control" id="display_order" name="display_order" value="{{ old('display_order') }}" placeholder="" onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+                                        <input 
+                                            type="text" 
+                                            class="form-control" 
+                                            id="display_order" 
+                                            name="display_order" 
+                                            value="{{ old('display_order') }}" 
+                                            onkeypress='return event.charCode >= 48 && event.charCode <= 57'
+                                        >
                                     </div>
                                     <button type="submit" class="btn btn-primary">Add</button>
                                 </div>
